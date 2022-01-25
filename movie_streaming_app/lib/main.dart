9eb2e1fddd702978fc1b32abb6c57e50ecf68e7e
Krystof Celba <krystof.celba@icloud.com>
2022-01-25 15:00:05 +0100

    Update the code to be more clear

--- movie_streaming_app/lib/main.dart
+++ movie_streaming_app/lib/main.dart
@@ -18,12 +18,24 @@ startServer() async {
       " On Port : " +
       server.port.toString());
   await for (var request in server) {
-    final json =
-        "[{top: 50.0, left: 66.95954487989886, width: 256.0809102402023, height: 337.6, color: #ffffffff}, {top: 397.6, left: 35.0, width: 105.0, height: 39.0, color: #ffffffff}]";
+    var statusCode = 405;
+    var responseJson =
+        '{"code": 405, "message": "No route found for \'${request.method} ${request.uri.path}\': Method Not Allowed"}';
+
+    if (request.method == "GET" &&
+        request.uri.path == "/getCurrentWidgetTree") {
+      statusCode == 200;
+      /******* REPLACE THE EXAMPLE JSON BELOW WITH A CALL TO YOUR METHOD FOR GETTING THE ACTUAL TREE *******/
+      responseJson =
+          "[{top: 50.0, left: 66.95954487989886, width: 256.0809102402023, height: 337.6, color: #ffffffff}, {top: 397.6, left: 35.0, width: 105.0, height: 39.0, color: #ffffffff}]"; // eg. getElementTreeJson();
+      /******* REPLACE THE EXAMPLE JSON ABOVE WITH A CALL TO YOUR METHOD FOR GETTING THE ACTUAL TREE *******/
+    }
+
     request.response
+      ..statusCode = statusCode
       ..headers.contentType =
           new ContentType("application", "json", charset: "utf-8")
-      ..write(json)
+      ..write(responseJson)
       ..close();
   }
 }
@@ -31,8 +43,7 @@ startServer() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    child:
-    MaterialApp(
+    return MaterialApp(
       title: 'Movie App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
